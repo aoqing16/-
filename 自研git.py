e2b7a7e47a4b3e073914a9605ab6959b329682a8
aoqing16 <wangchuanbing027@gmail.com>
2026-04-25 14:23:39 +0800

    自研脚本推送

--- 自研git.py
+++ 自研git.py
@@ -3,7 +3,7 @@ import subprocess
 a = r"C:\Users\ZhuanZ1\Desktop\多平台信息订阅"
 os.chdir(a)
 subprocess.run(['git','add','.'],shell=True,check=True)
-subprocess.run(['git','commit','-m','换文件夹后的推送更新'],shell=True,check=True)
+subprocess.run(['git','commit','-m','自研脚本推送'],shell=True,check=True)
 subprocess.run(['git','push','-f','origin','main'],shell=True,check=True)
 print('提交成功')
 # 这次报错我觉得原因应该是因为熔断机制的原因，第7行想把暂存区提交到本地仓库，但是发现没有东西交，于是报错
